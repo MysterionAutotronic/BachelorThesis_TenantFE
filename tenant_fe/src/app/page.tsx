783e36a291d4bb140fe9bbd105bf98d728d8b738
David Linhardt <divad2012@web.de>
2025-07-09 12:45:05 +0200

    works

--- tenant_fe/src/app/page.tsx
+++ tenant_fe/src/app/page.tsx
@@ -1,8 +1,54 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import { useConfig } from '@/lib/ConfigProvider';
+import styles from './page.module.css';
+import { type Config } from '@mystiker123/config-schema';
 
 export default function Home() {
-    return (
-        <div>Hello world</div>
-    );
+    const cfg = useConfig();
+
+    let loc: Intl.Locale | undefined = undefined;
+    if(cfg.address?.country) {
+        loc = new Intl.Locale(cfg.address.country);
+    }
+
+    function countryName(): string | null {
+        if (!loc) return null;
+        const countryEn = new Intl.DisplayNames([loc.language], { type: 'region'});
+        const res = countryEn.of(loc.region!);
+        if (!res) return null;
+        return res;
+    }
+
+    return(
+        <main>
+            <div className={styles.center}>
+                <h1 className={styles.companyName}>{cfg.companyName}</h1>
+                <p>{cfg.proposition}</p>
+            </div>
+                {
+                    cfg.products ?
+                        <div className={styles.productsDiv}>
+                            <h2>Products</h2>
+                            <ul>
+                                {cfg.products.map(p => <li key={p}>{p}</li>)}
+                            </ul>
+                        </div>
+                    :
+                        null
+                }
+            <div className={styles.addressDiv}>
+                <h2 className={styles.addressHeader}>Address</h2>
+                <address>
+                    {cfg.address?.street} {cfg.address?.streetNumber} <br/>
+                    {cfg.address?.zipCode} {cfg.address?.city} <br/>
+                    {countryName()}
+                </address>
+            </div>
+            <div className={styles.aboutDiv}>
+                <h2 className={styles.aboutHeader}>About us</h2>
+                <p>{cfg.about}</p>
+            </div>
+        </main>
+    )
 }
---
+++ tenant_fe/src/lib/ConfigProvider.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React, { createContext, useContext } from 'react';
+import { type Config } from '@mystiker123/config-schema';
+
+const ConfigCtx = createContext<Config | null>(null);
+
+export function useConfig(): Config {
+    const ctx = useContext(ConfigCtx);
+    if (!ctx) throw new Error('useConfig must be inside <ConfigProvider />');
+    return ctx;
+}
+
+export default function ConfigProvider(
+    { cfg, children }: { cfg: Config; children: React.ReactNode },
+) {
+    return <ConfigCtx.Provider value={cfg}>{children}</ConfigCtx.Provider>
+}
