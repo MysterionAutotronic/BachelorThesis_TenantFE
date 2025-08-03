24730cc4948fde3ee3d610849a7c0868f4f6cfe3
David Linhardt <divad2012@web.de>
2025-08-03 19:42:30 +0200

    remove env access from caching

--- tenant_fe/src/app/layout.tsx
+++ tenant_fe/src/app/layout.tsx
@@ -4,8 +4,7 @@ import { ConfigSchema } from '@mystiker123/config-schema';
 import ConfigProvider from '@/lib/ConfigProvider';
 
 async function loadConfig() {
-    const base = process.env.TENANT_BE_URL!;
-    const res  = await fetch(`${base}/config`, { next: { revalidate: 60*10 } });
+    const res  = await fetch(`/config`, { next: { revalidate: 60*10 } });
     if (!res.ok) throw new Error('Failed to load config');
     const json = await res.json();
     const parsed = ConfigSchema.parse(json);
